be3540415b18317fc6511d46975ddc4ac9c06816
brettfloyd <brfloyd19@gmail.com>
2024-12-18 21:51:09 -0500

    added lexer stuff

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,3 +11,13 @@ func New(input string) *Lexer { // New returns a new Lexer instance
 	l := &Lexer{input: input}
 	return l
 }
+
+func (l *Lexer) readChar() { // readChar reads the next character in the input and advances the position in the input string
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
