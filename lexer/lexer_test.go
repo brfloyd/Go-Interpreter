81ecf44e918ee28746756c7118b2853c90b4b8e8
brettfloyd <brfloyd19@gmail.com>
2024-12-22 18:30:57 -0500

    got to failing test on page 25

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,7 +7,14 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+    let ten = 10;
+    let add = fn(x, y) {
+      x + y;
+    };
+    let result = add(five, ten);
+  `
+
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -76,19 +83,4 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 
-}
-
-func testNextToken(t *testing.T) {
-	input := `let five = 5;
-    let ten = 10;
-    let add = fn(x, y) {
-      x + y;
-    };
-    let result = add(five, ten);
-  `
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	} // [...]
 }
